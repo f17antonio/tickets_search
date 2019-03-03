74540fbcc303431544a6196f5606aa02c7cd9187
Anton Grechnev <f17antonio@gmail.com>
2019-03-04 02:54:20 +0400

    Refactoring because redis module don't implement ZADD NX option.

--- src/search.rs
+++ src/search.rs
@@ -24,6 +24,11 @@ pub struct SearchResultSolution {
     pub price: i32
 }
 
+struct ArrivalData {
+    pub arrival_code: String,
+    pub arrival_time: i32
+}
+
 impl Search {
     pub fn search(con: &Connection, departure_code: String, arrival_code: String, departure_date: String, limit: i32) -> SearchResult {
         let departure_time = get_timestamp_from_str(departure_date);
@@ -36,13 +41,16 @@ impl Search {
 
 fn get_all_tickets(con: &Connection, departure_code: String, arrival_code: String, departure_time: i64) -> Vec<Vec<Ticket>>{
     let mut tickets : Vec<Vec<Ticket>> = Vec::new();
-    let direct_tickets : HashMap<String, i32> = get_tickets_by_time(con, departure_code,departure_time, departure_time + 86400);
+    let direct_tickets : HashMap<String, i32> = get_tickets_by_time(con, &departure_code,departure_time, departure_time + 86400);
     if direct_tickets.len() > 0 {
-        for (dirrect_arrival_code, departure_time) in direct_tickets {
-            if dirrect_arrival_code == arrival_code {
-                tickets.push(vec![get_ticket_by_time(con,&dirrect_arrival_code, &departure_time)]);
+        for (dirrect_arrival_data_string, departure_time) in direct_tickets {
+            let direct_arrival_data = get_arrival_data(dirrect_arrival_data_string);
+
+            if direct_arrival_data.arrival_code == arrival_code {
+                tickets.push(vec![get_ticket_by_time(con, &direct_arrival_data.arrival_code, &departure_time)]);
             } else {
-                let transfer_tickets = get_transfer_tickets(con, &arrival_code, dirrect_arrival_code, departure_time);
+                let transfer_tickets =
+                    get_transfer_tickets(con, &arrival_code, direct_arrival_data.arrival_code, direct_arrival_data.arrival_time, departure_time);
                 tickets.extend(transfer_tickets);
             }
         }
@@ -50,18 +58,18 @@ fn get_all_tickets(con: &Connection, departure_code: String, arrival_code: Strin
     return tickets;
 }
 
-fn get_transfer_tickets(con: &Connection, arrival_code: &String, dirrect_arrival_code: String, departure_time: i32) -> Vec<Vec<Ticket>> {
+fn get_transfer_tickets(con: &Connection, arrival_code: &String, dirrect_arrival_code: String, arrival_time: i32, departure_time: i32) -> Vec<Vec<Ticket>> {
     let mut transfer_tickets: Vec<Vec<Ticket>> = Vec::new();
-    let direct_ticket = get_ticket_by_time(con,&dirrect_arrival_code, &departure_time);
-    let start_time = direct_ticket.arrival_time as i64 + 10800;
-    let end_time = direct_ticket.arrival_time as i64 + 82800;
-    let tickets_with_transfer = get_tickets_by_time(con, dirrect_arrival_code, start_time, end_time);
+    let start_time = arrival_time as i64 + 10800;
+    let end_time = arrival_time as i64 + 82800;
+    let tickets_with_transfer = get_tickets_by_time(con, &dirrect_arrival_code, start_time, end_time);
     if tickets_with_transfer.len() > 0 {
-        for (transfer_arrival_code, transfer_departure_time) in tickets_with_transfer {
-            if &transfer_arrival_code == arrival_code {
+        for (transfer_arrival_data_string, transfer_departure_time) in tickets_with_transfer {
+            let transfer_arrival_data = get_arrival_data(transfer_arrival_data_string);
+            if &transfer_arrival_data.arrival_code == arrival_code {
                 transfer_tickets.push(vec![
-                    direct_ticket.clone(),
-                    get_ticket_by_time(con,&transfer_arrival_code, &transfer_departure_time)
+                    get_ticket_by_time(con, &dirrect_arrival_code, &departure_time),
+                    get_ticket_by_time(con, &transfer_arrival_data.arrival_code, &transfer_departure_time)
                 ]);
             }
         }
@@ -69,7 +77,7 @@ fn get_transfer_tickets(con: &Connection, arrival_code: &String, dirrect_arrival
     return transfer_tickets;
 }
 
-fn get_tickets_by_time(con: &Connection, departure_code: String, start_time: i64, end_time: i64) -> HashMap<String, i32> {
+fn get_tickets_by_time(con: &Connection, departure_code: &String, start_time: i64, end_time: i64) -> HashMap<String, i32> {
     let key = format!("times_{}", departure_code);
     let result: HashMap<String, i32> = con.zrangebyscore_withscores(key, start_time, end_time).unwrap();
     return result;
@@ -114,6 +122,13 @@ fn prepare_tickets(all_tickets: Vec<Vec<Ticket>>, limit: i32) -> Vec<SearchResul
     prepared_tickets.truncate(limit as usize);
 
     return prepared_tickets;
+}
+
+fn get_arrival_data(arrival_data_string: String) -> ArrivalData {
+    let arrival_data: Vec<&str> = arrival_data_string.split('_').collect();
+    let arrival_code = arrival_data[0].to_string();
+    let arrival_time = arrival_data[1].parse::<i32>().unwrap();
+    return ArrivalData{arrival_code, arrival_time};
 }
 
 fn get_timestamp_from_str(date_string : String) -> i64 {
--- src/tickets.rs
+++ src/tickets.rs
@@ -19,7 +19,8 @@ pub struct Ticket {
 
 impl Ticket {
     pub fn insert(con: &Connection, ticket: Ticket) {
-        let _: () = con.zadd(format!("times_{}", ticket.departure_code), &ticket.arrival_code, ticket.departure_time).unwrap();
+        let sorted_set_value = format!("{}_{}", ticket.arrival_code, ticket.arrival_time);
+        let _: () = con.zadd(format!("times_{}", ticket.departure_code), sorted_set_value, ticket.departure_time).unwrap();
         let _: () = con.set(format!("{}_{}", ticket.arrival_code, ticket.departure_time), &ticket.id).unwrap();
         let _: () = con.hset_multiple(format!("ticket_{}", ticket.id), &[
             ("id", ticket.id),
